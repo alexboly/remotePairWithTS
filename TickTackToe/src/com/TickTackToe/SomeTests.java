e3ff48ff4b5bf138fa5a03bcc836e27016930063
Tomasz Szymanski <szimano@Tomaszs-MacBook-Air.local>
2011-09-26 13:55:26 +0200

    first test for tictactoe

--- TickTackToe/src/com/TickTackToe/SomeTests.java
+++ TickTackToe/src/com/TickTackToe/SomeTests.java
@@ -4,10 +4,39 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class SomeTests {
-	
-	@Test
+
+    private static final String FIRST_PLAYER = "X";
+
+    @Test
 	public void Test(){
 		assertFalse(false);
 	}
 
+    @Test
+    public void emptyBoardOneByOne() {
+        // given
+        String table = "";
+
+        setStartingPlayer(FIRST_PLAYER);
+
+        // when
+        table = putTic(table);
+
+        // then
+        assertEquals(whoWon(table), FIRST_PLAYER);
+    }
+
+    private String firstPlayer;
+
+    private void setStartingPlayer(String firstPlayer) {
+        this.firstPlayer = firstPlayer;
+    }
+
+    private String whoWon(String table) {
+        return firstPlayer;
+    }
+
+    private String putTic(String table) {
+        return null;
+    }
 }
